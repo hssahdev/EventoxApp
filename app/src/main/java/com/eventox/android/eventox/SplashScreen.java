40cfd42e3691dd9d7784024a94ec9fa8b1e58f43
Harshdeep Singh <hssahdev1999@gmail.com>
2018-08-03 21:03:41 +0530

    first commmit

---
+++ app/src/main/java/com/eventox/android/eventox/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.eventox.android.eventox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static long splashscreenTimeout = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashscreenTimeout);
+    }
+}
