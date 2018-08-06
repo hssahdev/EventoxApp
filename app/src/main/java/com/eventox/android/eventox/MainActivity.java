6c5eb8ca688071a6938ffe82c45aa25b34023d6c
Harshdeep Singh <hssahdev1999@gmail.com>
2018-08-06 18:43:22 +0530

    Added Bottom navigation, added home fragment

--- app/src/main/java/com/eventox/android/eventox/MainActivity.java
+++ app/src/main/java/com/eventox/android/eventox/MainActivity.java
@@ -1,12 +1,11 @@
 package com.eventox.android.eventox;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,12 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.eventox.android.eventox.Navigation.HomeFragment;
 import com.eventox.android.eventox.User.UserActivity;
 
-public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener , HomeFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +39,35 @@ public class HomeActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//                        switch (menuItem.getItemId()){
-//                            case R.id.nav_calendar : return true;
-//                            case R.id.nav_home : return true;
-//                            case R.id.nav_explore : return true;
-//                            case R.id.nav_notifications : return true;
-//                        }
-                        return false;
-                    }
-                }
-        );
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener());
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener(){
+        BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.nav_calendar :
+                        return true;
+                    case R.id.nav_news:
+                        return true;
+                    case R.id.nav_home :
+                        getSupportFragmentManager().beginTransaction().add(R.id.mainFrameLayout, new HomeFragment()).commit();
+                        return true;
+                    case R.id.nav_explore :
+                        return true;
+                    case R.id.nav_notifications :
+                        return true;
+                }
+
+                return false;
+            }
+        };
+
+        return listener;
     }
 
     @Override
@@ -113,5 +126,10 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
